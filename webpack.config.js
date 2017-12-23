a209ae1e26750cf8b30e530ecc514bb68b00db34
sheldar <kewin.polok@gmail.com>
2017-12-23 19:29:21 +0100

    Minimal webpack config

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    entry: "./entry.js",
+    output: {
+        path: __dirname + "/build",
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: []
+    }
+};
