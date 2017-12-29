34e86538dd2c69ff7550856e9e5342c1b72c3fef
sheldar <kewin.polok@gmail.com>
2017-12-29 17:01:09 +0100

    Moved state to panel component from chart

--- src/components/chart/index.jsx
+++ src/components/chart/index.jsx
@@ -1,60 +1,13 @@
 import React from 'react';
 import { Line } from 'react-chartjs-2';
-import axios from 'axios';
-import moment from 'moment';
 
 class Chart extends React.Component {
     constructor(props) {
         super(props);
-
-        this.state = {
-            chartData: {
-                labels: [],
-                datasets: []
-            },
-            chartOptions: {},
-            intervalId: null
-        };
-    }
-
-    componentWillMount() {
-        const intervalId = setTimeout(() => { }, 1000);
-        this.updateData();
-
-        this.setState({
-            intervalId
-        });
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.state.intervalId);
-    }
-
-    updateData() {
-        axios
-            .get(`http://localhost:6060/api/measurements`)
-            .then((response) => {
-                this.setState({
-                    chartData: {
-                        labels: response.data.map((entry) => {
-                            return moment(entry.timestamp).format('HH:mm:ss');
-                        }),
-                        datasets: [{
-                            label: "Temperature Out",
-                            data: response.data.map((entry) => {
-                                return entry.temperature
-                            })
-                        }]
-                    }
-                })
-            })
-            .catch((error) => {
-                console.error(error);
-            });
     }
 
     render() {
-        return <Line data={this.state.chartData} options={this.state.chartOptions} />
+        return <Line data={this.props.data} />
     }
 };
 
--- src/components/panel/index.jsx
+++ src/components/panel/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Chart from '../chart/index.jsx';
+import axios from 'axios';
+import moment from 'moment';
 
 class Panel extends React.Component {
     constructor(props) {
@@ -7,36 +9,68 @@ class Panel extends React.Component {
         this.state = {
             fan: false,
             setpoint: null,
-            hysteresis: null
+            hysteresis: null,
+            measurementsChartData: {
+                labels: [],
+                datasets: []
+            }
         }
     }
+
+    componentWillMount() {
+        this.getMeasurements();
+    }
+
+    getMeasurements() {
+        axios
+            .get(`http://localhost:6060/api/measurements`)
+            .then((response) => {
+                this.setState({
+                    measurementsChartData: {
+                        labels: response.data.map((entry) => {
+                            return moment(entry.timestamp).format('HH:mm:ss');
+                        }),
+                        datasets: [{
+                            label: "Temperature Out",
+                            data: response.data.map((entry) => {
+                                return entry.temperature
+                            })
+                        }]
+                    }
+                })
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+
     render() {
         return (
-            <div class="container">
-                <div class="row">
-                    <div class="column">
+            <div className="container">
+                <div className="row">
+                    <div className="column">
                         <h1>heating-boiler-controller-plugin</h1>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="column">
+                <div className="row">
+                    <div className="column">
                         <span> Fan: { this.fan ? 'ON' : 'OFF' } </span>
                     </div>
-                    <div class="column">
+                    <div className="column">
                         <span> Setpoint: { this.state.setpoint } </span>
                     </div>
-                    <div class="column">
+                    <div className="column">
                         <span> Hysteresis: { this.state.hysteresis } </span>
                     </div>
-                    <div class="column">
+                    <div className="column">
                         <button type="button">
                             Refresh
                         </button>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="column">
-                        <Chart />
+                <div className="row">
+                    <div className="column">
+                        <Chart data={this.state.measurementsChartData} />
                     </div>
                 </div>
             </div>
