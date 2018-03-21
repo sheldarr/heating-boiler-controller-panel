f933460e3e10d652d7cdd1871f13aa99d6f27e02
sheldar <kewin.polok@gmail.com>
2018-03-21 17:56:58 +0100

    Enhanced layout for mobiles

--- entry.jsx
+++ entry.jsx
@@ -2,7 +2,6 @@ import ReactDOM from 'react-dom';
 import Panel from './src/components/panel/index.jsx';
 
 import 'normalize-css/normalize.css';
-import 'font-awesome/css/font-awesome.css';
 import 'bulma/css/bulma.css';
 
 ReactDOM.render(
--- src/components/panel/index.jsx
+++ src/components/panel/index.jsx
@@ -25,7 +25,7 @@ class Panel extends React.Component {
                 labels: [],
                 datasets: []
             },
-            lastMeasurements: 1440
+            lastMeasurements: 240
         }
 
         this.getMeasurements = this.getMeasurements.bind(this);
@@ -210,143 +210,121 @@ class Panel extends React.Component {
                 <section className="section">
                     <div className="container">
                         <div className="box">
-                            <div className="columns">
-                                <div className="column">
-                                    <div className="field">
-                                        <label className="label" htmlFor="mode">Mode</label>
-                                        <div className="control">
-                                            <div className="select">
-                                                <select
-                                                    className="select" 
-                                                    id="mode"
-                                                    value={this.state.mode}
-                                                    onChange={this.handleModeChange}
-                                                >
-                                                    <option value="NORMAL">NORMAL</option>
-                                                    <option value="FORCED_FAN_ON">FORCED_FAN_ON</option>
-                                                    <option value="FORCED_FAN_OFF">FORCED_FAN_OFF</option>
-                                                </select>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="column">
-                                    <div className="field">
-                                        <label className="label" htmlFor="setpoint">Setpoint</label>
-                                        <div className="control">
-                                            <input
-                                                className="input" 
-                                                id="setpoint"
-                                                type="number"
-                                                min="1"
-                                                max="100"
-                                                step="0.1"
-                                                value={this.state.setpoint}
-                                                onChange={this.handleSetpointChange}
-                                            />
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="column">
-                                    <div className="field">
-                                        <label className="label" htmlFor="hysteresis">Hysteresis</label>
-                                        <div className="control">
-                                            <input
-                                                className="input"
-                                                id="hysteresis"
-                                                type="number"
-                                                min="1"
-                                                max="10"
-                                                step="0.5"
-                                                value={this.state.hysteresis}
-                                                onChange={this.handleHysteresisChange} 
-                                            />
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="column">
-                                    <div className="field">
-                                        <label className="label" htmlFor="last-measurements">Last measurements</label>
-                                        <div className="control">
-                                            <input 
-                                                className="input"
-                                                id="last-measurements"
-                                                type="number"
-                                                value={this.state.lastMeasurements}
-                                                onChange={this.handleLastMeasurementsChange} 
-                                            />
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="column">
-                                    <div className="field">
-                                        <label className="label" htmlFor="power">Power</label>
-                                        <div className="control">
-                                            <input 
-                                                className="input"
-                                                id="power"
-                                                type="number"
-                                                min="0"
-                                                max="100"
-                                                value={this.state.power}
-                                                onChange={this.handlePowerChange} 
-                                            />
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="column">
-                                    <div className="buttons">
-                                        <button className="button is-primary" type="button" onClick={this.getHistoryMeasurements}>
-                                            <span className="icon is-small">
-                                                <i className="fas fa-refresh"></i>
-                                            </span>
-                                            <span>
-                                                refresh
-                                            </span>
-                                        </button>
-                                        <button className="button is-primary" type="button" onClick={this.saveSettings}>
-                                            <span className="icon is-small">
-                                                <i className="fas fa-check"></i>
-                                            </span>
-                                            <span>
-                                                Save
-                                            </span>
-                                        </button>
+                            <div className="field">
+                                <label className="label" htmlFor="mode">Mode</label>
+                                <div className="control">
+                                    <div className="select is-fullwidth">
+                                        <select
+                                            className="select" 
+                                            id="mode"
+                                            value={this.state.mode}
+                                            onChange={this.handleModeChange}
+                                        >
+                                            <option value="NORMAL">NORMAL</option>
+                                            <option value="FORCED_FAN_ON">FORCED_FAN_ON</option>
+                                            <option value="FORCED_FAN_OFF">FORCED_FAN_OFF</option>
+                                        </select>
                                     </div>
                                 </div>
                             </div>
+                            <div className="field">
+                                <label className="label" htmlFor="setpoint">Setpoint</label>
+                                <div className="control">
+                                    <input
+                                        className="input" 
+                                        id="setpoint"
+                                        type="number"
+                                        min="1"
+                                        max="100"
+                                        step="0.1"
+                                        value={this.state.setpoint}
+                                        onChange={this.handleSetpointChange}
+                                    />
+                                </div>
+                            </div>
+                            <div className="field">
+                                <label className="label" htmlFor="hysteresis">Hysteresis</label>
+                                <div className="control">
+                                    <input
+                                        className="input"
+                                        id="hysteresis"
+                                        type="number"
+                                        min="1"
+                                        max="10"
+                                        step="0.5"
+                                        value={this.state.hysteresis}
+                                        onChange={this.handleHysteresisChange} 
+                                    />
+                                </div>
+                            </div>
+                            <div className="field">
+                                <label className="label" htmlFor="last-measurements">Last measurements</label>
+                                <div className="control">
+                                    <input 
+                                        className="input"
+                                        id="last-measurements"
+                                        type="number"
+                                        value={this.state.lastMeasurements}
+                                        onChange={this.handleLastMeasurementsChange} 
+                                    />
+                                </div>
+                            </div>
+                            <div className="field">
+                                <label className="label" htmlFor="power">Power</label>
+                                <div className="control">
+                                    <input 
+                                        className="input"
+                                        id="power"
+                                        type="number"
+                                        min="0"
+                                        max="100"
+                                        value={this.state.power}
+                                        onChange={this.handlePowerChange} 
+                                    />
+                                </div>
+                            </div>
+                            <div className="field">
+                                <button className="button is-primary is-fullwidth" type="button" onClick={this.saveSettings}>
+                                    <span>
+                                        Save
+                                    </span>
+                                </button>
+                            </div>
+                            <div className="field">
+                                <button className="button is-primary is-fullwidth" type="button" onClick={this.getHistoryMeasurements}>
+                                    <span>
+                                        Refresh
+                                    </span>
+                                </button>
+                            </div>
                         </div>
                         <div className="box">
-                            <div className="columns">
-                                <div className="column">
-                                    <div className="field is-grouped is-grouped-multiline">
-                                        <div className="control">
-                                            <div className="tags has-addons">
-                                                <span className="tag is-info is-medium">Last sync</span>
-                                                <span className="tag is-light is-medium">{ this.state.lastMeasurementsData.timestamp.format('HH:mm:ss') }</span>
-                                            </div>
-                                        </div>
-                                        <div className="control">
-                                            <div className="tags has-addons">
-                                                <span className="tag is-info is-medium">FAN</span>
-                                                { this.state.fanOn 
-                                                    ? <span className="tag is-success is-medium">ON</span>
-                                                    : <span className="tag is-danger is-medium">OFF</span>
-                                                }
-                                            </div>
-                                        </div>
-                                        <div className="control">
-                                            <div className="tags has-addons">
-                                                <span className="tag is-info is-medium">Output</span>
-                                                <span className="tag is-light is-medium">{ this.state.lastMeasurementsData.output } ℃</span>
-                                            </div>
-                                        </div>
-                                        <div className="control">
-                                            <div className="tags has-addons">
-                                                <span className="tag is-info is-medium">Input</span>
-                                                <span className="tag is-light is-medium">{ this.state.lastMeasurementsData.input } ℃</span>
-                                            </div>
-                                        </div>
+                            <div className="field is-grouped">
+                                <div className="control">
+                                    <div className="tags has-addons">
+                                        <span className="tag is-info is-medium">Output</span>
+                                        <span className="tag is-light is-medium">{ this.state.lastMeasurementsData.output } ℃</span>
+                                    </div>
+                                </div>
+                                <div className="control">
+                                    <div className="tags has-addons">
+                                        <span className="tag is-info is-medium">Input</span>
+                                        <span className="tag is-light is-medium">{ this.state.lastMeasurementsData.input } ℃</span>
+                                    </div>
+                                </div>
+                                <div className="control">
+                                    <div className="tags has-addons">
+                                        <span className="tag is-info is-medium">FAN</span>
+                                        { this.state.fanOn 
+                                            ? <span className="tag is-success is-medium">ON</span>
+                                            : <span className="tag is-danger is-medium">OFF</span>
+                                        }
+                                    </div>
+                                </div>
+                                <div className="control">
+                                    <div className="tags has-addons">
+                                        <span className="tag is-info is-medium">Last sync</span>
+                                        <span className="tag is-light is-medium">{ this.state.lastMeasurementsData.timestamp.format('HH:mm:ss') }</span>
                                     </div>
                                 </div>
                             </div>
