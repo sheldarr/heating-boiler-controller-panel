0d21f14c1b57924b7b1c51308afb8d7821bbdaf5
sheldar <kewin.polok@gmail.com>
2019-11-04 20:01:21 +0100

    Initial new implementation

---
+++ README.md
@@ -0,0 +1,1 @@
+# heating-boiler-controller-panel
---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Head from 'next/head';
+
+const Home = (): JSX.Element => (
+  <div>
+    <Head>
+      <title>Home</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <h1>heating-boiler-controller-panel</h1>
+  </div>
+);
+
+export default Home;
