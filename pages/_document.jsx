1bc06eb2492bcba40725333602e51cfb587d6240
sheldar <kewin.polok@gmail.com>
2019-11-05 00:05:15 +0100

    Add styled components support

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,11 +1,32 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import React from 'react';
+import { ServerStyleSheet } from 'styled-components';
 
 class CustomDocument extends Document {
   static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
-    return { ...initialProps };
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
   }
 
   render() {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
+import Container from '@material-ui/core/Container';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import styled from 'styled-components';
+
+const StyledPaper = styled(Paper)`
+  padding: 2rem;
+`;
 
 const Home = () => {
   const [measurement, setMeasurement] = useState({});
@@ -20,8 +28,14 @@ const Home = () => {
         <title>heating-boiler-controller-panel</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>heating-boiler-controller-panel</h1>
-      <div>{JSON.stringify(measurement)}</div>
+      <Container>
+        <StyledPaper>
+          <Typography color="error" variant="h2" gutterBottom>
+            37.5 °C
+          </Typography>
+          {JSON.stringify(measurement)}
+        </StyledPaper>
+      </Container>
     </div>
   );
 };
