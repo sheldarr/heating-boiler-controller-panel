bf660787da8506155157a4dacf12ef2f6ab0dd36
sheldar <kewin.polok@gmail.com>
2019-11-04 22:08:54 +0100

    Websocket connection

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
 
-const Home = (): JSX.Element => (
-  <div>
-    <Head>
-      <title>Home</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <h1>heating-boiler-controller-panel</h1>
-  </div>
-);
+const Home = () => {
+  const [measurement, setMeasurement] = useState({});
+
+  useEffect(() => {
+    const websocket = new WebSocket('ws://localhost:3000/websocket');
+
+    websocket.onmessage = (event) => {
+      const freshMeasurement = JSON.parse(event.data);
+
+      setMeasurement(freshMeasurement);
+    };
+  }, []);
+
+  return (
+    <div>
+      <Head>
+        <title>heating-boiler-controller-panel</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>heating-boiler-controller-panel</h1>
+      <div>{JSON.stringify(measurement)}</div>
+    </div>
+  );
+};
 
 export default Home;
--- server.js
+++ server.js
@@ -2,14 +2,16 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
 const { createServer } = require('http');
+const url = require('url');
 const axios = require('axios');
 const next = require('next');
 const CronJob = require('cron').CronJob;
 const { createLogger, format, transports } = require('winston');
-const { combine, timestamp, prettyPrint } = format;
+const WebSocket = require('ws');
+const { combine, timestamp, simple } = format;
 
 const logger = createLogger({
-  format: combine(timestamp(), prettyPrint()),
+  format: combine(timestamp(), simple()),
   transports: [new transports.Console()],
 });
 
@@ -17,12 +19,20 @@ const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
+const webSocketServer = new WebSocket.Server({ noServer: true });
+
 new CronJob(
   process.env.CRON,
   async () => {
     logger.info(`REQUEST: ${process.env.CONTROLLER_URL}`);
 
     const { data } = await axios.get(process.env.CONTROLLER_URL);
+
+    webSocketServer.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify(data));
+      }
+    });
 
     logger.info('RESPONSE:', data);
   },
@@ -33,11 +43,24 @@ new CronJob(
 app.prepare().then(() => {
   createServer((req, res) => {
     handle(req, res);
-  }).listen(3000, (err) => {
-    if (err) {
-      throw err;
-    }
+  })
+    .on('upgrade', (request, socket, head) => {
+      logger.info(head);
+      const pathname = url.parse(request.url).pathname;
 
-    logger.info('> Ready on http://localhost:3000');
-  });
+      if (pathname === '/websocket') {
+        webSocketServer.handleUpgrade(request, socket, head, (ws) => {
+          webSocketServer.emit('connection', ws, request);
+        });
+      } else {
+        socket.destroy();
+      }
+    })
+    .listen(3000, (err) => {
+      if (err) {
+        throw err;
+      }
+
+      logger.info('> Ready on http://localhost:3000');
+    });
 });
