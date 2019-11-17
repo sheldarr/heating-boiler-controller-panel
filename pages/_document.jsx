3452fa71a3475a394fb3b4bfe71a1182a03614cf
sheldar <kewin.polok@gmail.com>
2019-11-17 21:10:58 +0100

    Fix SSR styles collecting

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,31 +1,37 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import React from 'react';
 import { ServerStyleSheet } from 'styled-components';
+import { ServerStyleSheets } from '@material-ui/core/styles';
 
 class CustomDocument extends Document {
   static async getInitialProps(ctx) {
-    const sheet = new ServerStyleSheet();
+    const styledComponentsStyleSheet = new ServerStyleSheet();
+    const materialUiStyleSheets = new ServerStyleSheets();
     const originalRenderPage = ctx.renderPage;
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
           enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
+            styledComponentsStyleSheet.collectStyles(
+              materialUiStyleSheets.collect(<App {...props} />)
+            ),
         });
 
       const initialProps = await Document.getInitialProps(ctx);
+
       return {
         ...initialProps,
         styles: (
           <>
             {initialProps.styles}
-            {sheet.getStyleElement()}
+            {materialUiStyleSheets.getStyleElement()}
+            {styledComponentsStyleSheet.getStyleElement()}
           </>
         ),
       };
     } finally {
-      sheet.seal();
+      styledComponentsStyleSheet.seal();
     }
   }
 
