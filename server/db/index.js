cd33805aa39f205c167a01b39ca697bfdf8ce957
sheldar <kewin.polok@gmail.com>
2019-11-19 19:40:58 +0100

    Keep last measurements

--- server/db/index.js
+++ server/db/index.js
@@ -6,6 +6,8 @@ const FileSync = require('lowdb/adapters/FileSync');
 const adapter = new FileSync('db.json');
 const db = low(adapter);
 
+const MAX_AMOUNT_OF_MEASUREMENTS = 3;
+
 db.defaults({
   'settings.fanOn': false,
   'settings.hysteresis': 2,
@@ -13,7 +15,9 @@ db.defaults({
   'settings.setpoint': 40,
   'status.lastSync': new Date(),
   'temperature.input': 0,
+  'temperature.inputMeasurements': [],
   'temperature.output': 0,
+  'temperature.outputMeasurements': [],
 }).write();
 
 const getStatus = () => {
@@ -24,13 +28,17 @@ const getStatus = () => {
   const mode = db.get('settings.mode').value();
   const outputTemperature = db.get('temperature.output').value();
   const setpoint = db.get('settings.setpoint').value();
+  const inputMeasurements = db.get('temperature.inputMeasurements').value();
+  const outputMeasurements = db.get('temperature.outputMeasurements').value();
 
   return {
     fanOn,
     hysteresis,
+    inputMeasurements,
     inputTemperature,
     lastSync,
     mode,
+    outputMeasurements,
     outputTemperature,
     setpoint,
   };
@@ -52,6 +60,32 @@ const setStatus = (
   db.set('status.lastSync', lastSync).write();
   db.set('temperature.input', inputTemperature).write();
   db.set('temperature.output', outputTemperature).write();
+
+  const inputMeasurements = db.get('temperature.inputMeasurements').value();
+
+  if (inputMeasurements.length >= MAX_AMOUNT_OF_MEASUREMENTS) {
+    inputMeasurements.shift();
+  }
+
+  inputMeasurements.push({
+    time: lastSync,
+    value: inputTemperature,
+  });
+
+  db.set('temperature.inputMeasurements', inputMeasurements).write();
+
+  const outputMeasurements = db.get('temperature.outputMeasurements').value();
+
+  if (outputMeasurements.length >= MAX_AMOUNT_OF_MEASUREMENTS) {
+    outputMeasurements.shift();
+  }
+
+  outputMeasurements.push({
+    time: lastSync,
+    value: outputTemperature,
+  });
+
+  db.set('temperature.outputMeasurements', outputMeasurements).write();
 };
 
 module.exports = {
