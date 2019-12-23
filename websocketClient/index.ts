e83920408ecbdf0ee0874a4ab300c157dad80672
sheldar <kewin.polok@gmail.com>
2019-12-23 22:07:38 +0100

    Replace native ws implementation

--- websocketClient/index.ts
+++ websocketClient/index.ts
@@ -1,46 +1,46 @@
-let ws;
+import Sarus from '@anephenix/sarus';
 
 const callbacksFns = [];
+let sarus = null;
+
+const initialize = () => {
+  if (!process.browser) {
+    return;
+  }
+  sarus = new Sarus({
+    eventListeners: {
+      close: [
+        () => {
+          console.log('Socket is closed.');
+        },
+      ],
+      error: [
+        (error) => {
+          console.error('Socket encountered error: ', error.message);
+        },
+      ],
+      message: [
+        (event) => {
+          callbacksFns.forEach((callback) => callback(event));
+        },
+      ],
+      open: [
+        () => {
+          console.log('Socket is open.');
+        },
+      ],
+    },
+    reconnectAutomatically: true,
+    url: `${location.protocol === 'https:' ? 'wss' : 'ws'}://${
+      location.host
+    }/websocket`,
+  });
+};
+
+initialize();
 
 export const registerCallback = (callback) => {
   callbacksFns.push(callback);
 };
 
-const connect = () => {
-  ws = new WebSocket(
-    `${location.protocol === 'https:' ? 'wss' : 'ws'}://${
-      location.host
-    }/websocket`
-  );
-
-  ws.onopen = function() {
-    console.log('Socket is open.');
-  };
-
-  ws.onmessage = (event) => {
-    callbacksFns.forEach((callback) => callback(event));
-  };
-
-  ws.onclose = function(e) {
-    console.log(
-      'Socket is closed. Reconnect will be attempted in 5 seconds.',
-      e.reason
-    );
-
-    setTimeout(function() {
-      connect();
-    }, 5000);
-  };
-
-  ws.onerror = function(err) {
-    console.error('Socket encountered error: ', err.message, 'Closing socket');
-
-    ws.close();
-  };
-};
-
-if (typeof window !== 'undefined') {
-  connect();
-}
-
-export default ws;
+export default sarus;
