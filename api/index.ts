3016a12d42d75bd5283321c60c78e5dc0e22eba6
sheldar <kewin.polok@gmail.com>
2020-02-25 22:10:07 +0100

    Controller status api and hook

---
+++ api/index.ts
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+type ControllerMode = 'FORCED_FAN_OFF' | 'FORCED_FAN_ON' | 'NORMAL';
+
+export type ControllerStatus = {
+  fanOn: boolean;
+  hysteresis: number;
+  inputTemperature: number;
+  lastSync: string;
+  mode: ControllerMode;
+  outputTemperature: number;
+  setpoint: number;
+};
+
+export const getControllerStatus = async () => {
+  return axios
+    .get<ControllerStatus>('/api/controller/status')
+    .then(({ data }) => data);
+};
